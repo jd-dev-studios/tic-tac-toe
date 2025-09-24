ea9f48feaccbe5b4d5005f9aaed6a39f722fe689
David Barszczak <david@davidbarszczak.com>
2025-09-24 22:50:40 +0200

    Added Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.davidbarszczak.tictactoe',
+  appName: 'tic-tac-toe',
+  webDir: 'dist-capacitor'
+};
+
+export default config;
